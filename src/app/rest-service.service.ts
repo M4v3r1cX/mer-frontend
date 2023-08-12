93adcad3e30ce58e65851e8d40733a85890edfb3
Maverick Vlasilev <m4v3r1cx.thanatos@gmail.com>
2023-08-12 00:12:14 -0400

    Se avanza con la wea

---
+++ src/app/rest-service.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestServiceService {
+  constructor() { }
+
+  // https://codingpotions.com/angular-servicios-llamadas-http
+  // https://codingpotions.com/angular-router
+  // https://codingpotions.com/angular-login-sesion/
+}
